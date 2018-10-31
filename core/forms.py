2f22d8c065eab816e59feb45df7cf24e527d47b5
Joffily Ferreira <joffily@outlook.com>
2018-10-31 14:11:06 -0300

    Adiciona exemplo de formulário com objetos dinâmicos

--- core/forms.py
+++ core/forms.py
@@ -6,3 +6,17 @@ class FuncionarioForm(forms.ModelForm):
     class Meta:
         model = Funcionario
         exclude = []
+
+    def __init__(self, *args, **kwargs):
+        self.setor = kwargs.pop('setor')
+        super(FuncionarioForm, self).__init__(*args, **kwargs)
+
+        if self.setor:
+            del self.fields['setor']
+
+    def clean(self, *args, **kwargs):
+        '''Valida o formulário'''
+        if self.instance:
+            # Atribuimos o setor a instância do form
+            self.instance.setor = self.setor
+        return super(FuncionarioForm, self).clean(*args, **kwargs)
--- core/urls.py
+++ core/urls.py
@@ -3,4 +3,6 @@ from core import views
 
 urlpatterns = [
     path('', views.register, name='register'),
+    path('register-by-sector/<int:setor_pk>/', views.register_with_sector,
+         name='register_with_sector'),
 ]
